98b16a4554678a40efe8929f5481936ca095eef7
Vladislav Chepesh <31590835+OnlyLonely32@users.noreply.github.com>
2019-12-02 18:40:03 +0300

    homework 13 +ultra

---
+++ project-homework13/src/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+
+gulp.task('hello', function(done){
+ console.log('Hello');
+ done();
+});
+// Static server
+gulp.task('browser-sync', function() {
+ browserSync.init({
+     server: {
+         baseDir: "./"
+     }
+ });
+ gulp.watch("./*.html").on('change', browserSync.reload);
+ gulp.watch("./css/*.css").on('change', browserSync.reload);
+});
+
+gulp.task('minify-css', () => {
+ return gulp.src('css/*.css')
+   .pipe(cleanCSS({compatibility: 'ie8'}))
+   .pipe(rename("style.min.css"))
+   .pipe(gulp.dest('../dist/css'));
+});
