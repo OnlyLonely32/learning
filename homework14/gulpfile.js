dfd81c1693544dcc3a12895fd98bf204e367bd3e
Vladislav Chepesh <31590835+OnlyLonely32@users.noreply.github.com>
2019-12-04 19:54:03 +0300

    feat: add normolize, grid, variables, header styles in template folder

--- homework14/gulpfile.js
+++ homework14/gulpfile.js
@@ -12,22 +12,22 @@ const sass = require('gulp-sass');
          baseDir: "src/"
      }
  })
- watch("src/*.html").on('change', browserSync.reload);
- watch("src/sass/**/*.sass", serveSass);
- watch("src/js/*.js").on('change', browserSync.reload);
+ watch("./src/*.html").on('change', browserSync.reload);
+ watch("./src/sass/**/*.sass", serveSass);
+ watch("./src/js/*.js").on('change', browserSync.reload);
 }
 
 function minifycss()  {
- return src('src/css/*.css')
+ return src('./src/css/*.css')
    .pipe(cleanCSS({compatibility: 'ie8'}))
    .pipe(rename("style.min.css"))
-   .pipe(dest('dist/css'));
+   .pipe(dest('./dist/css'));
 }
 
 function serveSass() {
-    return src("src/scss/*.scss")
+    return src("./src/sass/*.sass")
         .pipe(sass())
-        .pipe(dest("src/css"))
+        .pipe(dest("./src/css"))
         .pipe(browserSync.stream());
 }
 
