a8eb999b202290391d6ea1117c70daaf1a5b2261
Vladislav Chepesh <31590835+OnlyLonely32@users.noreply.github.com>
2019-12-17 21:43:19 +0300

    featc: add control section, animacion for section, slider in path section

--- homework14/src/js/main.js
+++ homework14/src/js/main.js
@@ -63,58 +63,75 @@ $(document).ready(function () {
   backToTop();
 
   let slider1 = new Swiper ('.slider1', {
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-  
+   uniqueNavElements: true,
+   pagination: {
+     el: '.swiper1__pagination',
+     clickable: true
+   },
+   navigation: {
+     nextEl: '.swiper1__next',
+     prevEl: '.swiper1__prev',
+   }
   });
   
   let slider2 = new Swiper ('.slider2', {
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
+   loop: true,
+   uniqueNavElements: true,
+   pagination: {
+     clickable: true,
+     el: '.swiper2__pagination'
+   },
+   navigation: {
+     nextEl: '.swiper2__next',
+     prevEl: '.swiper2__prev',
+   }
   });
 
   let next = $('.swiper1__next');
   let prev = $('.swiper1__prev');
   let bullets = $('.swiper1__pagination');
-
-  
   bullets.css('left', prev.width() + 25);
   next.css('left', prev.width() + bullets.width() + 50);
 
   let next1 = $('.swiper2__next');
   let prev1 = $('.swiper2__prev');
   let bullets1 = $('.swiper2__pagination');
-
-  
   bullets1.css('left', prev1.width() + 25);
   next1.css('left', prev1.width() + bullets1.width() + 50);
+  
+  let item;
+  let items = document.querySelectorAll('.swiper-slide__rows');
 
+  let changeActive = function (i) {
+   console.log(i);
+   if (i=== -1) {
+    i = items.length - 1;
+   } else if (i > items.length){
+    i = 0;
+   }else
+     console.log(i);
+    item = items[i];
+    console.log(items[i]);
+    items.forEach((element) => {
+     element.classList.remove('swiper-slide__rows-active');
+    })
+    item.classList.add('swiper-slide__rows-active');
 
-  var galleryThumbs = new Swiper('.gallery-thumbs', {
-    slidesPerView: 6,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
+    slider2.forEach((element) => {
+     element.slideTo(i+1);
+    });
+   
+  };
+
+  slider2[0].on('slideChange', () =>{
+   changeActive(slider2[0].activeIndex - 1);
   });
-  var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 6,
-    thumbs: {
-      swiper: galleryThumbs
-    }
+
+  items.forEach((item, i) => {
+   item.addEventListener('click', () => {
+    changeActive(i);
+   });
   });
 
+  new WOW().init();
 });
