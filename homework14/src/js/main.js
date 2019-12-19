0ceed2fca5686db73db2be55ce943e3da8a647ba
Vladislav Chepesh <31590835+OnlyLonely32@users.noreply.github.com>
2019-12-19 17:06:10 +0300

    feat: add client section. End work in footer section. Add YaMaps in footer

--- homework14/src/js/main.js
+++ homework14/src/js/main.js
@@ -256,4 +256,36 @@ $(document).ready(function () {
    }
    
    );
+
+   ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [47.244734, 39.723227],
+            zoom: 13
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Наш офис',
+            balloonContent: 'Вход со двора'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '../img/map-pin.svg',
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+    myMap.geoObjects
+        .add(myPlacemark);
+});
 });
